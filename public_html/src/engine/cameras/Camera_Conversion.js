fc34468261ff4693ab271d83ec90b7f6301a53d3
Alex Duda <alexjduda@gmail.com>
2019-05-27 16:20:41 -0700

    More camera conversion methods.
    Now from WC to pixels!

--- public_html/src/engine/cameras/Camera_Conversion.js
+++ public_html/src/engine/cameras/Camera_Conversion.js
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 
+Camera.prototype.getPixelsSize = function (wcSize) {
+    return wcSize * this._getWCToPixelsRatio();
+};
+
 /**
  * @returns {Number} float representing the ratio from WC space (world units)
  *                   to DC space (pixels)
@@ -24,4 +28,18 @@ Camera.prototype.getWCPosition = function(pixelPosition) {
     vec3.sub(wcPos, pixelPosition, vpOrigin);
     vec3.scaleAndAdd(wcPos, bottomLeft, wcPos, this._getPixelsToWCRatio());
     return wcPos;
+};
+
+Camera.prototype.getPixelsPosition = function (wcPosition) {
+    var pixelsOrigin =
+            vec3.fromValues(
+                this.getViewportLeft(),
+                this.getViewportBottom(),
+                0);
+    var bottomLeft = vec3.fromValues(this.getWCLeft(), this.getWCBottom(), 0);
+
+    var pixelsPos = vec3.create();
+    vec3.sub(pixelsPos, wcPosition, bottomLeft);
+    vec3.scaleAndAdd(pixelsPos, pixelsOrigin, pixelsPos, this._getWCToPixelsRatio());
+    return pixelsPos;
 };
