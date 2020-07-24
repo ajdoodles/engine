3660dbacf74b1bfe5bbf8c3ef438d10dc3d04907
Alex Duda <alexjduda@gmail.com>
2020-07-24 17:13:00 +0200

    Added LineRenderable, it draws lines
    LineRenderable takes a start point, end point, and line width. Using these values and the color it inherits from Renderable, it creates a transform that draws our standard buffered square as a colored line with the appropriate width. Currently width is only in world units.
    
    TODO: Add logic to set line with in pixels.

---
+++ public_html/src/engine/renderables/LineRenderable.js
@@ -0,0 +1,71 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function LineRenderable(x1, y1, x2, y2) {
+    Renderable.call(this);
+    Renderable.prototype.setColor.call(this, [1.0, 0.0, 0.0, 1.0]);
+    Renderable.prototype._setShader.call(this, gEngine.DefaultResources.getLineShader());
+    
+    this.mStart = vec2.fromValues(0, 0);
+    this.mEnd = vec2.fromValues(0, 0);
+    this.mLineWidth = 1;
+    
+    if (x1 !== undefined ) {
+        this.setVertices(x1, y1, x2, y2);
+    }
+};
+gEngine.Core.inheritPrototype(Renderable, LineRenderable);
+
+LineRenderable.prototype.draw = function (camera) {
+    Renderable.prototype.draw.call(this, camera);
+};
+
+LineRenderable.prototype._calcXform = function () {
+    //(sx, sy) is the line's vector
+    var sx = this.mEnd[0] - this.mStart[0];
+    var sy = this.mEnd[1] - this.mStart[1];
+    
+    //line center
+    var cx = this.mStart[0] + (sx/2);
+    var cy = this.mStart[1] + (sy/2);
+    
+    var lineLength = Math.sqrt((sx*sx) + (sy*sy));
+       
+    var rotation = vec2.angle(vec2.fromValues(sx, sy), vec2.fromValues(1, 0));
+    
+    this.mXform.setPosition(cx, cy);
+    this.mXform.setWidth(lineLength);
+    this.mXform.setRotationRads(rotation);
+};
+
+LineRenderable.prototype.setVertices = function (x1, y1, x2, y2) {
+    this.mStart[0] = x1;
+    this.mStart[1] = y1;
+    this.mEnd[0] = x2;
+    this.mEnd[1] = y2;
+    this._calcXform();
+};
+
+LineRenderable.prototype.setStartVertex = function (x, y) {
+    this.mStart[0] = x;
+    this.mStart[1] = y;
+    this._calcXform();
+};
+
+LineRenderable.prototype.setEndVertex = function (x, y) {
+    this.mEnd[0] = x;
+    this.mEnd[1] = y;
+    this._calcXform();
+};
+
+LineRenderable.prototype.setLineWidth = function (width) {
+    this.mLineWidth = width;
+    this.mXform.setHeight(width);
+};
+
+LineRenderable.prototype.draw = function (camera) {
+    Renderable.prototype.draw.call(this, camera);
+};
