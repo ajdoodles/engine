d00058a7e85336e9c9be230af4d476b83feafa8a
Alex <alexjduda@gmail.com>
2022-10-14 10:40:40 -0400

    Add an Asteroid and a Ship
    So fucking cool.
    
    TriShip is a simple triangle with WASD movement and QE rotation. It doesn't shoot, it doesn't do much, and the lines are jagged.
    
    Asteroid renders an octagon shape (GeometryType.ASTEROID) with a random position, size, velocity, and rotation. They do not collide yet.

---
+++ src/game/objects/Asteroid.ts
@@ -0,0 +1,49 @@
+import { vec2 } from "gl-matrix";
+import GameObject from "../../engine/gameobjects/GameObject";
+import RigidRect from "../../engine/physics/RigidRect";
+import Renderable from "../../engine/renderables/Renderable";
+import { GeometryType } from "../../engine/shaders/Geometry";
+import Asteroids from "../scenarios/Asteroids";
+
+export default class Asteroid extends GameObject {
+    
+    physicsComponent: RigidRect;
+
+    shape = GeometryType.ASTEROID;
+    rotation = (Math.random() * 10) - 5;
+
+    constructor(){
+        const renderable = new Renderable();
+        const randX = Math.random() * 100;
+        const randY = Math.random() * 75;
+        const randScale = Math.random() * 20;
+        const randVx = (Math.random() * 18) - 9;
+        const randVy = (Math.random() * 18) - 9;
+        renderable.xform.setPosition(randX, randY);
+        renderable.xform.setSize(randScale, randScale);
+        renderable.setColor(Asteroids.objectWhite);
+        super(renderable);
+        this.physicsComponent = new RigidRect(renderable.xform, renderable.xform.getWidth(), renderable.xform.getHeight());
+        this.physicsComponent.velocity = vec2.fromValues(randVx, randVy);
+    }
+
+    update() {
+        super.update();
+
+        const worldWidth = 100;
+        const worldHeight = 75;
+        const shipPos = this.xform.position;
+        if (shipPos[0] < 0) {
+            shipPos[0] =  worldWidth; 
+        } else if (shipPos[0] > worldWidth) {
+            shipPos[0] = 0;
+        }
+        if (shipPos[1] < 0) {
+            shipPos[1] = worldHeight; 
+        } else if (shipPos[1] > worldHeight) {
+            shipPos[1] = 0;
+        }
+        this.renderComponent.xform.incRotationInDegrees(this.rotation);
+    }
+
+}
---
+++ src/game/objects/TriShip.ts
@@ -0,0 +1,71 @@
+import { vec2 } from "gl-matrix";
+import GameObject from "../../engine/gameobjects/GameObject";
+import RigidRect from "../../engine/physics/RigidRect";
+import Renderable from "../../engine/renderables/Renderable";
+import { GeometryType } from "../../engine/shaders/Geometry";
+import Asteroids from "../scenarios/Asteroids";
+import input from "../../engine/core/Engine_Input";
+
+export default class TriShip extends GameObject {
+    physicsComponent !: RigidRect;
+    shape = GeometryType.TRIANGLE;
+
+    constructor(xPos:number, yPos: number) {
+        const triShip = new Renderable();
+        triShip.xform.setPosition(xPos, yPos);
+        triShip.xform.setSize(5, 10);
+        triShip.setColor(Asteroids.objectWhite);
+        super(triShip);
+
+        const xform = this.renderComponent.xform;
+        this.physicsComponent = new RigidRect(xform, xform.getWidth(), xform.getHeight());
+    }
+
+    update() {
+        const deltaV = 0.75;
+        const newV = vec2.clone(this.physicsComponent.velocity);
+        if (input.isKeyPressed(input.keys.W)) {
+          if (newV[1] < 4) {
+            newV[1] += deltaV;
+          }
+        }
+        if (input.isKeyPressed(input.keys.A)) {
+          if (newV[0] > -4) {
+            newV[0] -= deltaV;
+          }
+        }
+        if (input.isKeyPressed(input.keys.S)) {
+          if (newV[1] > -4) {
+            newV[1] -= deltaV;
+          }
+        }
+        if (input.isKeyPressed(input.keys.D)) {
+          if (newV[0] < 4) {
+            newV[0] += deltaV;
+          }
+        }
+        if (input.isKeyPressed(input.keys.Q)) {
+          this.xform.incRotationInDegrees(1);
+        }
+        if (input.isKeyPressed(input.keys.E)) {
+          this.xform.incRotationInDegrees(-1);
+        }
+        this.physicsComponent.velocity = newV;
+    
+        super.update();
+
+        const worldHeight = 75;
+        const worldWidth = 100;
+        const shipPos = this.xform.position;
+        if (shipPos[0] < 0) {
+            shipPos[0] =  worldWidth; 
+        } else if (shipPos[0] > worldWidth) {
+            shipPos[0] = 0;
+        }
+        if (shipPos[1] < 0) {
+            shipPos[1] = worldHeight; 
+        } else if (shipPos[1] > worldHeight) {
+            shipPos[1] = 0;
+        }
+      }
+}
