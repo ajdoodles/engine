ece322ec26322bf4eb0748eef96fc3e04ed0db9d
Alex <alexjduda@gmail.com>
2022-10-14 10:42:04 -0400

    Latest iteration of Asteroids testing scene
    This isn't a real commit I'm just moving files between computers.

--- src/game/scenarios/Asteroids.ts
+++ src/game/scenarios/Asteroids.ts
@@ -1,22 +1,30 @@
 import Scene from "../../engine/Scene";
 import fonts from "../../engine/core/resources/Engine_Fonts";
-import resources from "../../engine/core/resources/Engine_ResourceMap";
 import FontRenderable from "../../engine/renderables/FontRenderable";
 import Camera from "../../engine/cameras/Camera";
 import GL from "../../engine/core/Engine_GL";
 import { vec2 } from "gl-matrix";
+import TriShip from "../objects/TriShip";
+import GameObjectSet from "../../engine/gameobjects/GameObjectSet";
+import Asteroid from "../objects/Asteroid";
 
 export default class Asteroids extends Scene {
 
-    private static readonly spaceBlack: color = [0, 0, 0, 1];
-    private static readonly objectWhite: color = [1, 1, 1, 1];
+    static readonly spaceBlack: color = [0, 0, 0, 1];
+    static readonly objectWhite: color = [1, 1, 1, 1];
 
     private static readonly font = "assets/fonts/Consolas-72";
 
     private mainCamera !: Camera;
 
     private startMessage !: FontRenderable;
+    private triShip !: TriShip;
     
+    private asteroidSet !: GameObjectSet;
+
+    private readonly worldHeight = 75;
+    private readonly worldWidth = 100;
+
     loadScene(): void {
         fonts.loadFont(Asteroids.font);
     }
@@ -26,8 +34,7 @@ export default class Asteroids extends Scene {
     }
 
     initialize(): void {
-
-        const wcCenter = vec2.fromValues(50, 32.5);
+        const wcCenter = vec2.fromValues(this.worldWidth/2, this.worldHeight/2);
         const wcWidth = 100;
         const viewBounds: bounds = [0, 0, 640, 480];
         this.mainCamera = new Camera(wcCenter, wcWidth, viewBounds);
@@ -38,16 +45,31 @@ export default class Asteroids extends Scene {
         this.startMessage.getXform().setPosition(wcCenter[0], wcCenter[1]);
         this.startMessage.setTextHeight(4);
         this.startMessage.setColor(Asteroids.objectWhite);
+
+        this.triShip = new TriShip(wcCenter[0] - 5, wcCenter[1]);
+
+        this.asteroidSet = new GameObjectSet();
+        const numAsteroids = 6;
+        for (let i = 0; i < numAsteroids; i++) {
+            this.asteroidSet.addObject(new Asteroid());
+        }
+        this.asteroidSet.setAlertCollisions(false);
     }
 
     update(): void {
         this.mainCamera.setupViewProjection();
         this.mainCamera.update();
+
+        this.triShip.update();
+        this.asteroidSet.update();
     }
 
     draw(): void {
         GL.clearCanvas(Asteroids.spaceBlack);
 
+       this.triShip.draw(this.mainCamera);
         this.startMessage.draw(this.mainCamera);
+
+        this.asteroidSet.draw(this.mainCamera);
     }
 }
