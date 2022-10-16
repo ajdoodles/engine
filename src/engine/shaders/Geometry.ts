654c561d4c203b079120efa9e05025560330933d
Alex <alexjduda@gmail.com>
2022-10-16 11:44:39 -0400

    Remove dead, wrong, code.
    Geometry.ts had some extra types and interfaces from when I was trying to understand discriminated unions. The code I'm removing is unreachable, and was wrong anyway.

--- src/engine/shaders/Geometry.ts
+++ src/engine/shaders/Geometry.ts
@@ -1,20 +1,3 @@
-interface Square {
-    type: GeometryType.SQUARE,
-    vertices: 4,
-}
-
-interface Triangle {
-    type: GeometryType.TRIANGLE,
-    vertices: 3,
-}
-
-interface Asteroid {
-    type: GeometryType.ASTEROID,
-    vertices: 8,
-}
-
-type Geometry = Square | Triangle | Asteroid;
-
 export enum GeometryType {
     SQUARE,
     TRIANGLE,
